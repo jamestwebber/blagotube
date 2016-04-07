9a36c14c305c4d60b585db566b168aa7c1894c6f
James Webber <jamestwebber@gmail.com>
2016-04-07 11:43:28 -0700

    added README, new blog post on glasseye mods. Oh and I modified Glasseye a bunch

---
+++ README.md
@@ -0,0 +1,7 @@
+### Blagotube!
+
+That's the name I decided to give this project (based on [this comic](https://xkcd.com/181/)).
+
+This repository is the source code for [my website](http://ja.meswebber.com), which is a blog primarily focused on the topic of building itself. Because of the way [Lektor](https://www.getlektor.com) works, this code isn't hosted in the GitHub pages repository for the site&mdash;that just contains the compiled static version.
+
+The main appeal of this repository is that it contains my Lektor-plugin-adaptation of [Glasseye](http://coppeliamla.github.io/glasseye/glasseye_markdownExample/demo.html), a nice little tool for combining Markdown, Tufte CSS, and d3.js in one website. I've modified Simon's code to implement fancier sidenotes and some other tweaks.
--- packages/glasseye/lektor_glasseye.py
+++ packages/glasseye/lektor_glasseye.py
@@ -12,7 +12,7 @@ def glasseye_to_html(text):
         contents = to_wrap.replace_with(wrap_in)
         wrap_in.append(contents)
 
-#     #Function to add charts
+#     # Function to add charts
 #     def add_chart(chart_id, code_string):
 #         for d in enumerate(soup.findAll(chart_id)):
 #             code_string += chart_id + "(" + str(d[1].contents[0]) + ", '#" + chart_id + "_" + str(d[0])
@@ -27,7 +27,7 @@ def glasseye_to_html(text):
 #             d[1].insert_after(tag)
 #         return code_string
 
-    #Get paths and file names
+    # Get paths and file names
 #     user_path = os.getcwd() + "/"
 #     input_file = sys.argv[1]
 #     glasseye_path = os.path.dirname(os.path.abspath(__file__)) + "/"
@@ -36,43 +36,75 @@ def glasseye_to_html(text):
 
 #    tufte_template = "templates/tufteTemplate.html"
 
-
     # Convert markdown to html using pandoc
     converted_html = py.convert(text, 'html', format='md', extra_args=['--mathjax'])
 
     # soupify it
     soup = BeautifulSoup(converted_html, 'html.parser')
 
-    #Make the changes for the Tufte format
-    for a in soup.findAll('marginnote'):
-        p = soup.new_tag("p")
-        a.replaceWith(p)
-        p.insert(0, a)
+    # Make the changes for the Tufte format
+
+    # marginnotes
+    # <label for="mn-demo" class="margin-toggle">&#8853;</label>
+    # <input type="checkbox" id="mn-demo" class="margin-toggle"/>
+    # <span class="marginnote">
+    #   This is a margin note. Notice there isn’t a number preceding the note.
+    # </span>
+
+    for i,a in enumerate(soup.findAll('marginnote')):
+        lbl = soup.new_tag("label")
+        lbl['for'] = "mn-{}".format(i)
+        lbl['class'] = "margin-toggle"
+        lbl.string = u"⊕"
+
+        chkbox = soup.new_tag("input")
+        chkbox['type'] = "checkbox"
+        chkbox['id'] = "mn-{}".format(i)
+        chkbox['class'] = "margin-toggle"
+
+        a.insert_before(lbl)
+        a.insert_before(chkbox)
+
         a.name = "span"
         a['class'] = "marginnote"
 
-    for a in enumerate(soup.findAll('sidenote')):
-        a[1].name = "span"
-        a[1]['class'] = "marginnote"
-        a[1].insert(0, str(a[0]+1) + ". ")
-        tag = soup.new_tag("sup")
-        tag.string = str(a[0]+1)
-        a[1].insert_before(tag)
+    # sidenotes:
+    # <label for="sn-demo" class="margin-toggle sidenote-number"></label>
+    # <input type="checkbox" id="sn-demo" class="margin-toggle"/>
+    # <span class="sidenote">
+    #   This is a sidenote.
+    # </span>
+
+    for i,a in enumerate(soup.findAll('sidenote')):
+        lbl = soup.new_tag("label")
+        lbl['for'] = "sn-{}".format(i)
+        lbl['class'] = "margin-toggle sidenote-number"
+
+        chkbox = soup.new_tag("input")
+        chkbox['type'] = "checkbox"
+        chkbox['id'] = "sn-{}".format(i)
+        chkbox['class'] = "margin-toggle"
+
+        a.insert_before(lbl)
+        a.insert_before(chkbox)
+
+        a.name = "span"
+        a['class'] = "sidenote"
 
     for a in soup.findAll('checklist'):
         l = a.parent.findNext('ul')
         l['class'] = "checklist"
         a.extract()
 
-    if soup.ol != None:
+    if soup.ol is not None:
         for ol in soup.findAll('ol'):
-           if ol.parent.name != 'li':
-               wrap(ol, soup.new_tag("div", **{'class':'list-container'}))
+            if ol.parent.name != 'li':
+                wrap(ol, soup.new_tag("div", **{'class': 'list-container'}))
 
-    if soup.ul != None:
-       for ul in soup.findAll('ul'):
-           if ul.parent.name != 'li':
-               wrap(ul, soup.new_tag("div", **{'class':'list-container'}))
+    if soup.ul is not None:
+        for ul in soup.findAll('ul'):
+            if ul.parent.name != 'li':
+                wrap(ul, soup.new_tag("div", **{'class': 'list-container'}))
 
 
 #     #Process the charts
@@ -104,7 +136,6 @@ def glasseye_to_html(text):
 #         tag = soup.new_tag("br")
 #         d[1].insert_after(tag)
 
-
     return unicode(soup)
 
 
@@ -130,7 +161,5 @@ class GlasseyePlugin(Plugin):
     description = u'Adds modified Glasseye field type to Lektor.'
 
     def on_setup_env(self, **extra):
-        # Derives type name "glasseye" from class name.
-        print self.env
-        print dir(self.env)
+        # Derives type name "Glasseye" from class name.
         self.env.types[self.name] = GlasseyeDocType
